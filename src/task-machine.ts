7093a27c0a0763638bc62a2e8aa1083e897e7c85
Geo Galagaran <geo.galagaran@gmail.com>
2021-04-24 10:19:30 +0800

    Task machine can now create/disregard a task

--- src/task-machine.ts
+++ src/task-machine.ts
@@ -1,32 +1,15 @@
+import { nanoid } from "nanoid";
 import create from "zustand/vanilla";
+import { MODE, TASK_TYPE } from "./types/enums";
+import { Key, Task, TaskMachineState } from "./types/interfaces";
+import { insertAtIndex, removeAtIndex } from "./utils";
 
-export enum MODE {
-  NORMAL,
-  INSERT,
-}
-
-export enum TASK_TYPE {
-  DRAFT,
-  ONGOING,
-  FINISHED,
-}
-
-interface Key {
-  return: boolean;
-  escape: boolean;
-}
-
-interface Task {
-  id: string;
-  name: string;
-  type: TASK_TYPE;
-}
-
-interface TaskMachineState {
-  tasks: Task[];
-  currentRow: number;
-  currentCol: number;
-  mode: MODE;
+export function createTask(taskType: TASK_TYPE, taskName = ""): Task {
+  return {
+    id: nanoid(),
+    name: taskName,
+    type: taskType,
+  };
 }
 
 export const DEFAULT_TASK_MACHINE_STATE: TaskMachineState = {
@@ -36,7 +19,7 @@ export const DEFAULT_TASK_MACHINE_STATE: TaskMachineState = {
   mode: MODE.NORMAL,
 };
 
-export const taskMachine = create((set, get) => ({
+export const taskMachine = create<TaskMachineState>((set, get) => ({
   ...DEFAULT_TASK_MACHINE_STATE,
   transition(input: string, key: Key): void {
     switch (get().mode) {
@@ -59,7 +42,15 @@ export const taskMachine = create((set, get) => ({
             break;
 
           case input === "o":
-            set(() => ({ mode: MODE.INSERT }));
+            set((state) => ({
+              mode: MODE.INSERT,
+              currentRow: state.currentRow + 1,
+              tasks: insertAtIndex(
+                state.tasks,
+                state.currentRow + 1,
+                createTask(TASK_TYPE.DRAFT)
+              ),
+            }));
             break;
 
           default:
@@ -71,8 +62,79 @@ export const taskMachine = create((set, get) => ({
 
       case MODE.INSERT: {
         switch (true) {
-          case key.escape:
-            set(() => ({ mode: MODE.NORMAL }));
+          case key?.escape:
+            set((state) => {
+              const nextCurrentRow = state.currentRow - 1;
+              return {
+                mode: MODE.NORMAL,
+                tasks: removeAtIndex(state.tasks, state.currentRow),
+                currentRow: nextCurrentRow < 0 ? 0 : nextCurrentRow,
+              };
+            });
+            break;
+
+          case key?.shift && key?.return:
+            set((state) => {
+              const draftTask = state.tasks[state.currentRow];
+
+              if (!draftTask.name) {
+                return state;
+              }
+
+              const newTasks = insertAtIndex(
+                state.tasks,
+                state.currentRow,
+                createTask(TASK_TYPE.ONGOING, draftTask.name)
+              );
+
+              const nextCurrentRow = state.currentRow + 1;
+              newTasks[nextCurrentRow].name = "";
+
+              return {
+                tasks: newTasks,
+                currentRow: nextCurrentRow,
+              };
+            });
+            break;
+
+          case key?.return:
+            set((state) => {
+              const draftTask = state.tasks[state.currentRow];
+
+              if (!draftTask.name) {
+                return state;
+              }
+
+              const nextCurrentRow = state.currentRow - 1;
+              const newTasks = insertAtIndex(
+                state.tasks,
+                state.currentRow,
+                createTask(TASK_TYPE.ONGOING, draftTask.name),
+                true
+              );
+
+              return {
+                mode: MODE.NORMAL,
+                tasks: newTasks,
+                currentRow: nextCurrentRow < 0 ? 0 : nextCurrentRow,
+              };
+            });
+            break;
+
+          default:
+            set((state) => {
+              const draftTask = state.tasks[state.currentRow];
+              const newTasks = insertAtIndex(
+                state.tasks,
+                state.currentRow,
+                {
+                  ...draftTask,
+                  name: draftTask.name + input,
+                },
+                true
+              );
+              return { tasks: newTasks };
+            });
             break;
         }
         break;
--- tests/task-machine.test.js
+++ tests/task-machine.test.js
@@ -1,8 +1,13 @@
+import { nanoid } from "nanoid";
 import {
   taskMachine,
+  createTask,
   DEFAULT_TASK_MACHINE_STATE,
-  MODE,
 } from "../src/task-machine";
+import { MODE, TASK_TYPE } from "../src/types/enums";
+import { getRandomIndex, insertAtIndex } from "../src/utils";
+
+const randomString = nanoid();
 
 describe("Task Machine", () => {
   afterEach(() => {
@@ -66,7 +71,143 @@ describe("Task Machine", () => {
 
     setState({ mode: MODE.INSERT });
     expect(getState().mode).toBe(MODE.INSERT);
-    transition(null, { escape: true });
+    transition("", { escape: true });
     expect(getState().mode).toBe(MODE.NORMAL);
+  });
+
+  it("Should insert a draft task on the next row when transitioning from NORMAL to INSERT", () => {
+    const { getState, setState } = taskMachine;
+    const { transition } = getState();
+
+    const sampleTasks = Array(5);
+    const randomIndex = getRandomIndex(sampleTasks);
+
+    setState({ tasks: sampleTasks, currentRow: randomIndex });
+    transition("o");
+
+    const nextRow = randomIndex + 1;
+    const insertedTaskAtRandomIndex = getState().tasks[nextRow];
+
+    expect(getState().tasks.length).toBe(sampleTasks.length + 1);
+    expect(getState().currentRow).toBe(nextRow);
+    expect(insertedTaskAtRandomIndex.type).toBe(TASK_TYPE.DRAFT);
+    expect(insertedTaskAtRandomIndex.name).toBe("");
+  });
+
+  it("Should be able to update the draft task's name during insert mode", () => {
+    const { getState, setState } = taskMachine;
+    const { transition } = getState();
+
+    const sampleTasks = Array(5);
+    const randomIndex = getRandomIndex(sampleTasks);
+
+    setState({
+      tasks: insertAtIndex(
+        sampleTasks,
+        randomIndex,
+        createTask(TASK_TYPE.DRAFT)
+      ),
+      currentRow: randomIndex,
+      mode: MODE.INSERT,
+    });
+
+    transition(randomString);
+
+    const taskAtRandomIndex = getState().tasks[randomIndex];
+    expect(taskAtRandomIndex.name).toBe(randomString);
+  });
+
+  it("Should not commit a draft task when inputted with 'ESCAPE' during INSERT mode", () => {
+    const { getState, setState } = taskMachine;
+    const { transition } = getState();
+
+    const sampleTasks = Array(5);
+    const randomIndex = getRandomIndex(sampleTasks);
+
+    setState({
+      tasks: sampleTasks,
+      currentRow: randomIndex,
+    });
+
+    transition("o");
+    transition(randomString);
+
+    const draftTask = getState().tasks[randomIndex + 1];
+    expect(draftTask.name).toBe(randomString);
+
+    transition("", { escape: true });
+    expect(getState().tasks).toEqual(sampleTasks);
+  });
+
+  it("Should be able to commit a draft task when it contains a name and 'RETURN' is inputted during INSERT mode", () => {
+    const { getState, setState } = taskMachine;
+    const { transition } = getState();
+
+    const sampleTasks = Array(5);
+    const randomIndex = getRandomIndex(sampleTasks);
+
+    setState({
+      tasks: sampleTasks,
+      currentRow: randomIndex,
+    });
+
+    transition("o");
+    transition(randomString);
+    transition("", { return: true });
+
+    expect(getState().tasks.length).toBe(sampleTasks.length + 1);
+
+    const committedTask = getState().tasks[randomIndex + 1];
+    expect(committedTask.name).toEqual(randomString);
+    expect(committedTask.type).toEqual(TASK_TYPE.ONGOING);
+  });
+
+  it("Commits a draft task then creates a new draft task right afterwards during INSERT mode when 'SHIFT + RETURN' is inputted", () => {
+    const { getState, setState } = taskMachine;
+    const { transition } = getState();
+
+    const sampleTasks = Array(5);
+    const randomIndex = getRandomIndex(sampleTasks);
+
+    setState({
+      tasks: sampleTasks,
+      currentRow: randomIndex,
+    });
+
+    transition("o");
+    transition(randomString);
+    transition("", { shift: true, return: true });
+
+    expect(getState().tasks.length).toBe(sampleTasks.length + 2);
+
+    const committedTask = getState().tasks[randomIndex + 1];
+    expect(committedTask.name).toEqual(randomString);
+    expect(committedTask.type).toEqual(TASK_TYPE.ONGOING);
+
+    const draftTask = getState().tasks[randomIndex + 2];
+    expect(draftTask.name).toEqual("");
+    expect(draftTask.type).toEqual(TASK_TYPE.DRAFT);
+  });
+
+  it("Should not commit a draft task when it contains an empty name", () => {
+    const { getState, setState } = taskMachine;
+    const { transition } = getState();
+
+    const sampleTasks = Array(5);
+    const randomIndex = getRandomIndex(sampleTasks);
+
+    setState({
+      tasks: sampleTasks,
+      currentRow: randomIndex,
+    });
+
+    transition("o");
+    transition("", { return: true });
+
+    expect(getState().tasks.length).toBe(sampleTasks.length + 1);
+
+    const draftTask = getState().tasks[randomIndex + 1];
+    expect(draftTask.name).toEqual("");
+    expect(draftTask.type).toEqual(TASK_TYPE.DRAFT);
   });
 });
