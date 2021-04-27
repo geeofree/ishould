2c4913b1763e4c3bb70fb7d20e941547ab30a6c1
Geo Galagaran <geo.galagaran@gmail.com>
2021-04-27 17:59:34 +0800

    Add task machine front end representation

---
+++ src/main.tsx
@@ -0,0 +1,4 @@
+import React from "react";
+import { render } from "ink";
+import App from "./task-ui";
+render(<App title="TODO" />);
---
+++ src/task-ui.tsx
@@ -0,0 +1,109 @@
+import React, { ReactElement } from "react";
+import { Box, Text, useInput } from "ink";
+import { useTaskMachine } from "./task-machine";
+import { Task } from "./types/interfaces";
+import { TASK_TYPE } from "./types/enums";
+
+interface TaskItemProps {
+  task: Task;
+  isFocused: boolean;
+  currentCol: number;
+}
+
+function TaskItem(props: TaskItemProps): ReactElement {
+  const { task, currentCol, isFocused } = props;
+
+  let taskName: string | ReactElement[] = task.name;
+
+  if (task.type === TASK_TYPE.DRAFT || task.type === TASK_TYPE.DRAFT_ONGOING) {
+    taskName = (task.name + " ").split("").map((c, i) => (
+      <Text key={i} inverse={i === currentCol}>
+        {c}
+      </Text>
+    ));
+  }
+
+  let taskTypeIndicator: string;
+  switch (task.type) {
+    case TASK_TYPE.DRAFT:
+    case TASK_TYPE.DRAFT_ONGOING:
+      taskTypeIndicator = " * ";
+      break;
+
+    case TASK_TYPE.ONGOING:
+      taskTypeIndicator = "[ ]";
+      break;
+
+    case TASK_TYPE.FINISHED:
+      taskTypeIndicator = "[x]";
+      break;
+  }
+
+  return (
+    <Box marginBottom={1}>
+      <Box alignItems="flex-start">
+        <Text color="cyanBright" bold>
+          {isFocused ? "\u279c" : " "}
+        </Text>
+      </Box>
+
+      <Box marginX={1} alignItems="flex-start">
+        <Text dimColor={task.type === TASK_TYPE.FINISHED}>
+          {taskTypeIndicator}
+        </Text>
+      </Box>
+
+      <Box flexGrow={1}>
+        <Text
+          dimColor={task.type === TASK_TYPE.FINISHED}
+          strikethrough={task.type === TASK_TYPE.FINISHED}
+        >
+          {taskName}
+        </Text>
+      </Box>
+    </Box>
+  );
+}
+
+function Tasks(): ReactElement {
+  const taskMachine = useTaskMachine();
+  const { tasks, transition, currentRow, currentCol } = taskMachine;
+
+  useInput(transition);
+
+  if (tasks.length === 0) {
+    return <Text color="red">No tasks created yet</Text>;
+  }
+
+  return (
+    <Box flexDirection="column">
+      {tasks.map((task, index) => (
+        <TaskItem
+          key={task.id}
+          task={task}
+          isFocused={currentRow === index}
+          currentCol={currentCol}
+        />
+      ))}
+    </Box>
+  );
+}
+
+interface AppProps {
+  title: string;
+}
+
+function App(props: AppProps): ReactElement {
+  const { title } = props;
+  return (
+    <Box padding={1} margin={1} borderStyle="bold" flexDirection="column">
+      <Box marginBottom={1}>
+        <Text color="magenta">{title}</Text>
+      </Box>
+
+      <Tasks />
+    </Box>
+  );
+}
+
+export default App;
